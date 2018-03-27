ddf2c0a5b05713cd955db597f4dc335928723740
restrellado <ryan.estrellado@gmail.com>
2018-03-27 14:44:14 -0700

    Add documentation

--- join_ela_math.R
+++ join_ela_math.R
@@ -1,19 +1,22 @@
 join_ela_math <- function(e_data, m_data, dist_name) {
-  library(tidyverse)
-  library(forcats)
+  # Joins CA Dashboard ELA and Math data and anonymizes district 
+  #  Args: 
+  #    e_data: ELA dashboard dataset path
+  #    m_data: Math dashboard dataset path 
+  #    dist_name: character string to use in anonymization 
+  #  Returns: a tibble
+  ela <- readr::read_tsv(e_data) 
+  math <- readr::read_tsv(m_data) 
   
-  ela <- read_tsv(e_data) 
-  math <- read_tsv(m_data) 
-  
-  inner_join(ela, math, by = c(
+  dplyr::inner_join(ela, math, by = c(
     "schoolname" = "schoolname", "studentgroup" = "studentgroup"
   )) %>% 
-    select(districtname = districtname.x, 
-           schoolname, 
-           studentgroup, 
-           currstatus_ela = currstatus.x, 
-           currstatus_math = currstatus.y) %>% 
-    mutate(districtname = dist_name, 
-           schoolname = as.factor(schoolname), 
-           schoolname = fct_anon(schoolname, "school_")) 
+    dplyr::select(districtname = districtname.x, 
+                  schoolname, 
+                  studentgroup, 
+                  currstatus_ela = currstatus.x, 
+                  currstatus_math = currstatus.y) %>% 
+    dplyr::mutate(districtname = dist_name, 
+                  schoolname = as.factor(schoolname), 
+                  schoolname = forcats::fct_anon(schoolname, "school_")) 
 } 
